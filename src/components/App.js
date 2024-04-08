690f2c7027fd7202d97156001d7afcfbaa2c6cf0
HeyGirl <my@email.com>
2024-04-08 13:54:27 +0300

    Added a video

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { NavBar } from './NavBar'
+import "./App.css";
+import YoutubeEmbed from "./YoutubeVideo";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <YoutubeEmbed embedId="HQ8H5gqGA34" />
     </div>
   );
 }
